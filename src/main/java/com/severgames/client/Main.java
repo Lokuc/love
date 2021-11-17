4bc8dce3aadf765d075d8a91ede30fcededa58bf
Lokuc <udonov.kirya.21@gmail.com>
2021-11-17 19:45:50 +0200

    i

--- src/main/java/com/severgames/client/Main.java
+++ src/main/java/com/severgames/client/Main.java
@@ -1,7 +1,7 @@
 package com.severgames.client;
 
 public class Main {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         ClientWindow clientWindow = new ClientWindow();
     }
 }
